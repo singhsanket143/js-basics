b599f81dd7fd9757a4253520dde390fabde6f51d
singh.sanket1103 <singh.sanket1103@gmail.com>
2024-06-17 17:00:50 +0000

    added data types and variable name rules

---
+++ datatypersdemo.js
@@ -0,0 +1,8 @@
+let productPrice = 150000;
+
+let productDiscount = 15; 
+
+let productName = "Apple Iphone 11 Pro max (512 GB, Blue)";
+
+let isAvailable = true;
+
---
+++ variablename.js
@@ -0,0 +1,8 @@
+let first$name = "Sanket";
+
+let phone1 = 9999999999;
+
+let phone2 = 9090909090;
+
+
+console.log(first$name);
