3b4db992da179019972353b6e31b9e6a0c7c969f
singh.sanket1103 <singh.sanket1103@gmail.com>
2024-06-19 16:46:19 +0000

    added arithmetic operators

---
+++ operators/arithmeticoperators.js
@@ -0,0 +1,16 @@
+let x = 2;
+let y = 3;
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+console.log(x % y);
+
+console.log(10 % 3);
+
+console.log(x ** y);
