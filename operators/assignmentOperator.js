99e802e9ad103ab1cd4b80f02bdc1bb2c7413884
singh.sanket1103 <singh.sanket1103@gmail.com>
2024-06-19 16:57:26 +0000

    assignment operators added

---
+++ operators/assignmentOperator.js
@@ -0,0 +1,25 @@
+let x = 10;
+
+x += 5;
+
+console.log(x);
+
+x -= 2;
+
+console.log(x);
+
+x *= 3;
+
+console.log(x);
+
+x /= 2;
+
+console.log(x);
+
+x %= 3;
+
+console.log(x);
+
+x **= 2;
+
+console.log(x);
