438cc17270fa7559d7c5e0b61b15f257e5670fe7
singh.sanket1103 <singh.sanket1103@gmail.com>
2024-06-19 17:14:27 +0000

    logical operators added

---
+++ operators/logicaloperators.js
@@ -0,0 +1,12 @@
+console.log(false && false);
+console.log(false && true);
+console.log(true && false);
+console.log(true && true);
+
+console.log(false || false);
+console.log(false || true);
+console.log(true || false);
+console.log(true || true);
+
+console.log(!false);
+console.log(!true);
