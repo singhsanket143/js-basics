b5476149e1a7c33a024b445c9ba6ac432e45ff55
singh.sanket1103 <singh.sanket1103@gmail.com>
2024-06-19 16:34:23 +0000

    Added new demo

--- datatypersdemo.js
+++ datatypersdemo.js
@@ -4,5 +4,19 @@ let productDiscount = 15;
 
 let productName = "Apple Iphone 11 Pro max (512 GB, Blue)";
 
-let isAvailable = true;
+let isAvailable = false;
 
+let minusZero = -0;
+
+console.log(minusZero);
+
+let x;
+
+let y = null;
+
+console.log(x);
+
+
+let veryBigNumber = 57n;
+
+console.log(veryBigNumber);
---
+++ objectdemo.js
@@ -0,0 +1,16 @@
+let product = {
+    name: "Apple Iphone 11 Pro max (512 GB, Blue)",
+    price: 150000,
+    discount: 15,
+    isAvailable: false
+};
+
+let socialHandles = {
+    fb: "null",
+    insta: 6700,
+    "linkedin": 2000,
+    x: 5000,
+    insta: 9000
+};
+
+console.log(socialHandles);
