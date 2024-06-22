d379cdf41b3e2e8e28e6c8cefd9fa127fcb650b4
singh.sanket1103 <singh.sanket1103@gmail.com>
2024-06-22 16:20:25 +0000

    added loops

---
+++ loops/whiledemo.js
@@ -0,0 +1,10 @@
+let i = 1;
+// while(i <= 100) {
+//     console.log(i);
+//     i += 1;
+// }
+
+do {
+    console.log(i);
+    i += 1;
+} while(i <= 100);
---
+++ operators/bitwiseoperators.js
@@ -0,0 +1,14 @@
+const a = 5;
+const b = 6;
+
+console.log(a & b); // bitwise and
+
+console.log(a | 4); // bitwise or
+
+console.log(a ^ b);
+
+console.log(1 << 4);
+
+console.log(27 >> 2);
+
+console.log(9 >> 1);
